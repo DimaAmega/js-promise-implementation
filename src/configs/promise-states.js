a3d75d1a0336a3ce50fd677dee8ba4c1e967d384
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2023-08-10 02:24:49 +0400

    Update promise-states.js

--- src/configs/promise-states.js
+++ src/configs/promise-states.js
@@ -1,6 +1,6 @@
 const PROMISE_STATES = {
   Fulfilled: 'Promise is Fulfilled',
   Pending: 'Promise is Pending',
-  Rejected: 'Promise is rejected',
+  Rejected: 'Promise is Rejected',
 }
 export default PROMISE_STATES
